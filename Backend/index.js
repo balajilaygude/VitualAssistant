99b7b8cdeeeeb479fb3676f2f800f4467bb72a66
Balaji Laygude <balajiwork05@gmail.com>
2026-06-05 13:48:30 +0530

    frontend Link Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ const port=process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://vitualassistant.onrender.com",
     credentials:true
 }))
 app.use("/api/auth",authRouter)
