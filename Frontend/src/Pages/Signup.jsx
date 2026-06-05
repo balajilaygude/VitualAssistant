7d5685cbe685d10b5e27737b556a8f92ebd36b1c
Balaji Laygude <balajiwork05@gmail.com>
2026-06-05 14:25:55 +0530

    Update Signup.jsx

--- Frontend/src/Pages/Signup.jsx
+++ Frontend/src/Pages/Signup.jsx
@@ -41,7 +41,7 @@ export default function Signup() {
   }
 
   return (
-    <div className="w-full h-screen bg-cover bg-center flex  bg-[url('/authBg.png')] justify-center items-center">
+    <div className="w-full h-screen bg-cover  bg-[url('/authBg.png')] bg-center flex justify-center items-center" >
       <form
         onSubmit={handleSignup}
         className="w-100 h-4/5 bg-blue-900/15 shadow-lg shadow-black rounded backdrop-blur-md
