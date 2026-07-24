7c3c4bb0c5f8db3266464bff2af66f321229212e
Balaji Laygude <balajiwork05@gmail.com>
2026-07-24 14:16:14 +0530

    Change in cors

--- Backend/index.js
+++ Backend/index.js
@@ -13,10 +13,11 @@ const port=process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    origin:"https://vitualassistant.onrender.com",
-    credentials:true
-}))
+// app.use(cors({
+//     origin:"https://vitualassistant.onrender.com",
+//     credentials:true
+// }))
+app.use(cors())
 app.use("/api/auth",authRouter)
 app.use("/api/user",userRouter)
 
